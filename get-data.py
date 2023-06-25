f2d2a4c6668ed383f3198f33db51f540e068a535
Luciousdev <lucy.puyenbroek@gmail.com>
2023-06-25 21:30:58 +0200

    Small changes
    - Added template for HTML report
    - Added colors for monitoring status
    - Ability to customize benchmark process

--- get-data.py
+++ get-data.py
@@ -3,6 +3,40 @@ import psutil
 import subprocess
 import statistics
 import time
+from datetime import datetime
+
+def prError(skk):print("\033[91m {}\033[00m" .format(skk))
+def prOk(skk):print("\033[92m {}\033[00m" .format(skk))
+def prYellow(skk):print("\033[93m {}\033[00m" .format(skk))
+def prInfo(skk):print("\033[94m {}\033[00m" .format(skk))
+
+
+def printResults(min_temperature, max_temperature, avg_temperature, interval):
+    now = datetime.now()
+    time = now.strftime("%Y-%m-%d-%H-%M-%S")
+    template = f"""
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PC Benchmark</title>
+</head>
+
+<body>
+    <h1>Benchmark results:</h1>
+    <p>Benchmark duration: {interval}sec <br>Maximum CPU temperature: {max_temperature}°C <br>Average CPU temperature: {avg_temperature}°C <br>Minimum CPU temperature: {min_temperature}°C </p>
+</body>
+
+</html>
+    """
+
+    write = open(time+".html", "w")
+    write.write(template)
+    write.close()
+    prOk(f"[OK] - HTML report generated: {time}.html")
+
 
 def get_cpu_temperature():
     try:
@@ -28,9 +62,10 @@ def get_cpu_temperature():
     return None
 
 
-def monitor_cpu_temperature(interval):
+def monitor_cpu_temperature(interval, utilization):
     temperatures = []
     start_time = time.time()
+    prInfo(f"[INFO] - Starting benchmark for {interval} seconds and CPU utilization of {utilization}%")
 
     while time.time() - start_time < interval:
         temperature = get_cpu_temperature()
@@ -43,19 +78,28 @@ def monitor_cpu_temperature(interval):
         max_temperature = max(temperatures)
         avg_temperature = statistics.mean(temperatures)
 
-        print(f"Maximum Temperature: {max_temperature} °C")
-        print(f"Average Temperature: {avg_temperature} °C")
-        print(f"Minimum Temperature: {min_temperature} °C")
+        prOk(f"[OK] - CPU test finished.")
+        prInfo("[INFO] - Generating HTML report...")
+        printResults(min_temperature, max_temperature, avg_temperature, interval)
     else:
         print("No temperature data available.")
 
 
-# ---- START OF SCRIPT ----
 if __name__ == '__main__':
-    interval = 20
-    utilization = 100 
+    interval = 0
+    utilization = 0 
+    userinput = input("Would you like to customize the benchmark? [y/n]")
+    if userinput == "n":
+        prInfo("[INFO] - Using default values...")
+        interval = 120
+        utilization = 100
+    else:
+        interval = int(input("Enter the benchmark duration in seconds: "))
+        utilization = int(input("Enter the CPU utilization in percentage: "))
+
+
     cpu_load_script_path = 'assets/loads/cpu-load.py' 
 
     cpu_load_process = subprocess.Popen(['python', cpu_load_script_path, str(interval), str(utilization)])
-    monitor_cpu_temperature(interval)
+    monitor_cpu_temperature(interval, utilization)
     cpu_load_process.terminate()
