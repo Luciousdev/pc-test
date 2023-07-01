40d1211b6e39d51c4ee5543bb7a6701bcd1e0305
Luciousdev <lucy.puyenbroek@gmail.com>
2023-07-01 12:20:14 +0200

    Updated windows bat script

--- install-windows.bat
+++ install-windows.bat
@@ -1,32 +1,76 @@
 @echo off
 
+rem Set the log file path
+set LOG_FILE=install.log
+
+rem Function to log the output to the log file
+:Log
+echo %* >> %LOG_FILE%
+echo %*
+exit /b
+
 rem Check if Python 3.11 is installed
-python --version 2>NUL | findstr /B "Python 3.11" >nul
+py -3.11 -c "exit()" >nul 2>&1
 if %errorlevel% equ 0 (
-    echo Python 3.11 is already installed.
+    call :Log Python 3.11 is already installed.
 ) else (
-    echo Python 3.11 is not installed. Installing...
+    call :Log Python 3.11 is not installed. Installing...
     
     REM Download Python 3.11 
-    echo Downloading Python 3.11 installer...
+    call :Log Downloading Python 3.11 installer...
     powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe', 'python-3.11.0-amd64.exe')"
     
-    echo Installing Python 3.11... This may take a while
-    python-3.11.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    rem Check if the download was successful
+    IF NOT EXIST python-3.11.0-amd64.exe (
+        call :Log Failed to download Python 3.11 installer.
+        exit /b 1
+    )
+    
+    call :Log Installing Python 3.11... This may take a while
+    start /wait python-3.11.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    
+    rem Check if the installation was successful
+    IF %errorlevel% neq 0 (
+        call :Log Failed to install Python 3.11.
+        exit /b 1
+    )
     
     REM Delete the installer
-    echo Deleting installer...
-    del python-3.11.0-amd64.exe
-
-    echo Python 3.11 has been installed.
+    call :Log Deleting installer...
+    del python-3.11.0-amd64.exe >nul 2>&1
+    
+    rem Check if the deletion was successful
+    IF %errorlevel% neq 0 (
+        call :Log Failed to delete the installer.
+        exit /b 1
+    )
+    
+    call :Log Python 3.11 has been installed.
 )
 
 rem Install requirements
+call :Log Installing requirements...
 pip install -r requirements.txt
 
-echo Requirements have been installed.
+rem Check if the installation was successful
+IF %errorlevel% neq 0 (
+    call :Log Failed to install requirements.
+    exit /b 1
+)
+
+call :Log Requirements have been installed.
 
 rem Execute get-data.py
+call :Log Executing get-data.py...
 python get-data.py
 
-echo Script execution completed.
+rem Check if the script execution was successful
+IF %errorlevel% neq 0 (
+    call :Log Failed to execute the script.
+    exit /b 1
+)
+
+call :Log Script execution completed.
+
+rem Exit the script
+exit /b 0
